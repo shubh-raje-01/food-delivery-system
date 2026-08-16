aea556157278362a154e4c1389a9d2e82ddd9392
shubzz-001 <sdraje01@gmail.com>
2026-08-16 20:08:44 +0530

    Initialize restaurant-service service with spring cloud discovery support

---
+++ restaurant-service/src/test/java/com/food_delivery_system/restaurant_service/RestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_delivery_system.restaurant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
