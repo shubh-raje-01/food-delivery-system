f7bb82e68c88a97485bb3dc898f2c16df5663be5
shubzz-001 <sdraje01@gmail.com>
2026-08-16 20:52:47 +0530

    Implement core restaurant service domain models and repositories

---
+++ restaurant-service/src/main/java/com/food_delivery_system/restaurant_service/model/OpeningHours.java
@@ -0,0 +1,4 @@
+package com.food_delivery_system.restaurant_service.model;
+
+public class OpeningHours {
+}
---
+++ restaurant-service/src/main/java/com/food_delivery_system/restaurant_service/repository/OpeningHoursRepository.java
@@ -0,0 +1,4 @@
+package com.food_delivery_system.restaurant_service.repository;
+
+public interface OpeningHoursRepository {
+}
