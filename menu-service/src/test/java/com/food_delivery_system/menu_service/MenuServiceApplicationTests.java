a95b3c55e8cf5d60e891cfcd552ec214ab4f650d
shubzz-001 <sdraje01@gmail.com>
2026-08-23 22:53:35 +0530

    Initialize new menu-service module and add repository to restaurant-service

---
+++ menu-service/src/test/java/com/food_delivery_system/menu_service/MenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_delivery_system.menu_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- restaurant-service/src/main/java/com/food_delivery_system/restaurant_service/repository/RestaurantRepository.java
+++ restaurant-service/src/main/java/com/food_delivery_system/restaurant_service/repository/RestaurantRepository.java
@@ -3,11 +3,12 @@ package com.food_delivery_system.restaurant_service.repository;
 import com.food_delivery_system.restaurant_service.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     Optional<Restaurant> findByUuid(String uuid);
-    Optional<Restaurant> findByOwnerUuid(String ownerUuid);
+    List<Restaurant> findByOwnerUuid(String ownerUuid);
 
 }
