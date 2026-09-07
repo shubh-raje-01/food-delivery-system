5afc2d29bdb6e008e0ea58e06e015c4a790ccd3e
shubzz-001 <sdraje01@gmail.com>
2026-09-07 21:07:29 +0530

    feat: initialize order-service project structure with Maven wrapper and Spring Boot configuration

---
+++ order-service/src/test/java/com/food_delivery_system/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_delivery_system.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
